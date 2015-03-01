257148096afb8b92f57b8cd314a8f2736fec257d
Honglongwu <wuhonglong.china@gmail.com>
2015-03-02 07:32:04 +0800

    one more practice  of ex1

---
+++ ex1m.py
@@ -0,0 +1,20 @@
+'''
+#=============================================================================
+#     FileName: ex1m.py
+#         Desc: more practice on excercise.
+#       Author: Honglong Wu
+#        Email: wuhonglong@genomics.cn
+#      Version: 0.0.1
+#      Created: 01/03/2015
+#      History:
+#=============================================================================
+'''
+
+#-*- coding: utf-8 -*-
+
+# print one more line
+print "Hello World!\n"
+# print all the content as one line
+print "Hello World!" + "Hello Again" + "..."
+
+
