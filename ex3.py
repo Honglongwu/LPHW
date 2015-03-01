2959fc7878688db3ec7313ddd349ee57a2e95f4e
Honglongwu <wuhonglong.china@gmail.com>
2015-03-02 07:52:50 +0800

    calculator

---
+++ ex3.py
@@ -0,0 +1,39 @@
+'''
+#=============================================================================
+#     FileName: ex3.py
+#         Desc: This program was used for testing 
+		the function of calculator
+#       Author: Honglong Wu
+#        Email: wuhonglong@genomics.cn
+#      Version: 0.0.1
+#      Created: 02/03/2015
+#      History:
+#=============================================================================
+'''
+
+#-*- coding: utf-8 -*-
+
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 -5 + 4 % 2 - 1.0 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print  3 + 2 < 5 - 7
+
+print "What is 3 + 2?",3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "oh, thit's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2 
+
