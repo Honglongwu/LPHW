a7042460d05d4e65f09df9cff05907fa98a8b13c
Honglongwu <wuhonglong.china@gmail.com>
2015-03-02 07:22:39 +0800

    ex1 program

---
+++ ex1.py
@@ -0,0 +1,23 @@
+'''
+#=============================================================================
+#     FileName: ex1.py
+#         Desc: First Script for Github just for Fun.
+#       Author: Honglong Wu
+#        Email: wuhonglong@genomics.cn
+#      Version: 0.0.1
+#      Created: 01/03/2015
+#      History:
+#=============================================================================
+'''
+
+#-*- coding: utf-8 -*-
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print "Yay! Printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+ 
+
