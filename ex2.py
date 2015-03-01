fedddd70411bb3059388b7075640d987137e8723
Honglongwu <wuhonglong.china@gmail.com>
2015-03-02 07:40:57 +0800

    Function on #

---
+++ ex2.py
@@ -0,0 +1,26 @@
+'''
+#=============================================================================
+#     FileName: ex2.py
+#         Desc: This program was used for testing 
+		the function of annotation & "#"
+#       Author: Honglong Wu
+#        Email: wuhonglong@genomics.cn
+#      Version: 0.0.1
+#      Created: 02/03/2015
+#      History:
+#=============================================================================
+'''
+
+#-*- coding: utf-8 -*-
+
+# A comment, this is so you can read your program latter
+# Anything after the # is ignored by python
+
+print "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "this won't run."
+
+print "This will run."
+ 
+
