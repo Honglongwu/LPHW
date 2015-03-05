cf55f1037fb1f5d9ab3e0f29d30823480a4cfac3
Honglongwu <wuhonglong.china@gmail.com>
2015-03-05 09:55:51 +0800

    t

---
+++ ex13.py
@@ -0,0 +1,25 @@
+'''
+#=============================================================================
+#     FileName: ex13.py 
+#         Desc: practice with arguments 
+#       Author: Honglong Wu
+#        Email: wuhonglong@genomics.cn
+#      Version: 0.0.1
+#      Created: 05/03/2015
+#      History:
+#=============================================================================
+'''
+
+#-*- coding: utf-8 -*-
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+
+
