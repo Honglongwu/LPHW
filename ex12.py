acaf261b8b7ca32df5739adfba27bb5d1b368fb7
Honglongwu <wuhonglong.china@gmail.com>
2015-03-05 09:53:41 +0800

    test

---
+++ ex12.py
@@ -0,0 +1,20 @@
+'''
+#=============================================================================
+#     FileName: ex12.py 
+#         Desc: recieve the input from keyboard and give some promot info 
+#       Author: Honglong Wu
+#        Email: wuhonglong@genomics.cn
+#      Version: 0.0.1
+#      Created: 05/03/2015
+#      History:
+#=============================================================================
+'''
+
+#-*- coding: utf-8 -*-
+age = raw_input("How old are you?")
+height = raw_input("How tall are you?")
+weight = raw_input("How much do you weight?")
+
+print "So you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+
