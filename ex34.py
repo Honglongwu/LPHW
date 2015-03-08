2e191a80a2d6a8290775a5de6d61f684eb0dc2c2
Honglongwu <wuhonglong.china@gmail.com>
2015-03-09 07:11:37 +0800

    list index

---
+++ ex34.py
@@ -0,0 +1,18 @@
+'''
+#=============================================================================
+#     FileName: ex34.py 
+#         Desc: list 
+#       Author: Honglong Wu
+#        Email: wuhonglong@genomics.cn
+#      Version: 0.0.1
+#      Created: 09/03/2015
+#      History:
+#=============================================================================
+'''
+
+#-*- coding: utf-8 -*-
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+for i in range(len(animals)):
+	print "The %d animal is at %d and is %s" %(i + 1, i, animals[i])
+
