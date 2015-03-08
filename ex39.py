a03599f87e8b6172565874b825fbca759815c9ae
Honglongwu <wuhonglong.china@gmail.com>
2015-03-09 07:41:13 +0800

    test

---
+++ ex39.py
@@ -0,0 +1,36 @@
+'''
+#=============================================================================
+#     FileName: ex39.py 
+#         Desc: list operation 
+#       Author: Honglong Wu
+#        Email: wuhonglong@genomics.cn
+#      Version: 0.0.1
+#      Created: 09/03/2015
+#      History:
+#=============================================================================
+'''
+
+#-*- coding: utf-8 -*-
+
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+
+print "Wait there's not ten things in that lists, let's fix that."
+
+stuff = ten_things.split(' ')
+more_stuff = ['Day', 'Night', 'Song', 'Frisbee', 'Corn', 'Banana', 'Girl', 'Boy']
+
+while len(stuff) != 10:
+	next_one = more_stuff.pop()
+	print "Adding: ", next_one
+	stuff.append(next_one)
+	print "There's %d items now." % len(stuff)
+
+print "There we go: ", stuff
+
+print "Let's do some things with stuff."
+
+print stuff[1]
+print stuff[-1]
+print stuff.pop()
+print ' '.join(stuff)
+print '#'.join(stuff[3:5])
