eb5b39e4d66c5aa61c6b5d1f51f94bdd62640833
Honglongwu <wuhonglong.china@gmail.com>
2015-03-12 06:53:11 +0800

    project

---
+++ Ex46/setup.py
@@ -0,0 +1,35 @@
+'''
+#=============================================================================
+#     FileName: setup.py 
+#         Desc: project of ex46 
+#       Author: Honglong Wu
+#        Email: wuhonglong@genomics.cn
+#      Version: 0.0.1
+#      Created: 12/03/2015
+#      History:
+#=============================================================================
+'''
+
+#-*- coding: utf-8 -*-
+
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+config = 
+{
+	'description': "Ex46",
+	'author': 'Honglong Wu',
+	'url': 'https://github.com/Honglongwu/LPHW',
+	'download_url': 'https://github.com/Honglongwu/LPHW',
+	'author_email': 'wuhonglong@genomics.cn',
+	'version': '0.1',
+	'install_requires': ['nose'],
+	'packages': ['Ex46'],
+	'scripts': [],
+	'name': 'projectname'
+}
+
+setup(**config)
+
