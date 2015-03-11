13beed463d46691b1f758bea14e52cf145cedd1b
Honglongwu <wuhonglong.china@gmail.com>
2015-03-12 06:59:56 +0800

    update

---
+++ Ex46/tests/Ex46_tests.py
@@ -0,0 +1,26 @@
+'''
+#=============================================================================
+#     FileName: Ex46_tests.py 
+#         Desc: project of ex46 
+#       Author: Honglong Wu
+#        Email: wuhonglong@genomics.cn
+#      Version: 0.0.1
+#      Created: 12/03/2015
+#      History:
+#=============================================================================
+'''
+
+#-*- coding: utf-8 -*-
+from nose.tools import *
+import Ex46
+
+def setup():
+	print "setup!"
+
+def teardown():
+	print "Tear Down!"
+
+def test_basic():
+	print "I RAN!"
+
+
