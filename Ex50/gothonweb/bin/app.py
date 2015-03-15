ea4b4b20eb29c1f0dbcd247c4d804d10c74040cb
Honglongwu <wuhonglong.china@gmail.com>
2015-03-16 07:36:51 +0800

    m

--- Ex50/gothonweb/bin/app.py
+++ Ex50/gothonweb/bin/app.py
@@ -25,9 +25,12 @@ render = web.template.render('/Users/user/bin/GitHub/LPHW/Ex50/gothonweb/templat
 
 class Index(object):
 	def GET(self):
-		form = web.input(name = "Nobody")
-		greeting = "Hello, %s" % form.name
-		return render.index(greeting = greeting)
+		form = web.input(greeting=None, name = "Nobody")
+		if form.greeting:
+			greeting = "%s, %s" % (form.greeting, form.name)
+			return render.index(greeting = greeting)
+		else:
+			return "Error: greet is required."
 
 if __name__ == "__main__":
 	app.run()
