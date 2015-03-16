2b98579f30c1cb0813ba0a7b6490a1da83bf11bd
Honglongwu <wuhonglong.china@gmail.com>
2015-03-17 07:14:33 +0800

    tests

---
+++ Ex50/gothonweb/tests/app_tests.py
@@ -0,0 +1,36 @@
+'''
+#=============================================================================
+#     FileName: app_tests.py 
+#         Desc: html game 
+#       Author: Honglong Wu
+#        Email: wuhonglong@genomics.cn
+#      Version: 0.0.1
+#      Created: 17/03/2015
+#      History:
+#=============================================================================
+'''
+
+#-*- coding: utf-8 -*-
+from nose.tools import *
+from bin.app import app
+from tests.tools import assert_response
+
+def test_index():
+	# check that we get a 404 on the / url
+	resp = app.request("/")
+	assert_response(resp, status = "404")
+
+	# test our first GET request to /hello
+	resp = app.request("/hello")
+	assert_response(resp)
+	
+	# make sure default values work for the form
+	resp = app.request("/hello", method="POST")
+	assert_response(resp, contains="Nobody")
+
+	# test that we get expected values 
+	data = {'name':'zed', 'greet':'Hola'}
+	resp = app.request("/hello", method="POST", data=data)
+	assert_response(resp, contains="Zed")
+
+	
