b34cf047607b154ef5ec15caa92c19e201744ccf
Honglongwu <wuhonglong.china@gmail.com>
2015-03-18 07:25:51 +0800

    update

---
+++ Ex50/gothonweb/gothonweb/map.py
@@ -0,0 +1,26 @@
+'''
+#=============================================================================
+#     FileName: map.py 
+#         Desc: One example 
+#       Author: Honglong Wu
+#        Email: wuhonglong@genomics.cn
+#      Version: 0.0.1
+#      Created: 17/03/2015
+#      History:
+#=============================================================================
+'''
+
+#-*- coding: utf-8 -*-
+
+class Room(object):
+	
+	def __init__(self, name, description):
+		self.name = name
+		self.description = description
+		self.paths = {}
+
+	def go(self, direction):
+		return self.paths.get(direction, None)
+
+	def add_paths(self, paths):
+		self.paths.update(paths)
---
+++ Ex50/gothonweb/tests/map_tests.py
@@ -0,0 +1,26 @@
+'''
+#=============================================================================
+#     FileName: map_tests.py 
+#         Desc: project of ex50 
+#       Author: Honglong Wu
+#        Email: wuhonglong@genomics.cn
+#      Version: 0.0.1
+#      Created: 17/03/2015
+#      History:
+#=============================================================================
+'''
+
+#-*- coding: utf-8 -*-
+from nose.tools import *
+import gothonweb
+
+def setup():
+	print "setup!"
+
+def teardown():
+	print "Tear Down!"
+
+def test_basic():
+	print "I RAN!"
+
+
