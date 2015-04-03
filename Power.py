fca77507db164fe5132b990907a83e95326e21c4
Honglongwu <wuhonglong.china@gmail.com>
2015-04-04 07:57:44 +0800

    poer

---
+++ Power.py
@@ -0,0 +1,21 @@
+'''
+#=============================================================================
+#     FileName: Power.py 
+#         Desc: iterate
+#       Author: Honglong Wu
+#        Email: wuhonglong@genomics.cn
+#      Version: 0.0.1
+#      Created: 04/04/2015
+#      History:
+#=============================================================================
+'''
+
+#-*- coding: utf-8 -*-
+def Power(x, n):
+	if n == 0:
+		return 1
+	else:
+		return x * Power(x, n - 1)
+	
+num = int(raw_input('which number do you want? '))
+print Power(2, num)
